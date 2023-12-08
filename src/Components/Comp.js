85316af8604732b7df1163674405667c06bc5785
DESKTOP-OTQA2NF\E-TIME <fsafdar627@gmail.com>
2023-12-07 23:16:00 -0800

    ,navBar

---
+++ src/Components/Comp.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+
+export default class Comp extends Component {
+    render()
+    {
+        return (
+            <>
+            <div>Compiiii</div>
+            <h1>{this.props.name}</h1>
+            </>
+          
+          )
+    }
+ 
+}
+
